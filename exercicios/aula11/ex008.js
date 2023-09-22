d87025cba39e5020bdd2499eaf33169d977b1372
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2023-09-22 15:41:58 -0300

    Criei os ex011 e o ex001

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`Á velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60){ // condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Itália'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil', 'brasil') {
+    console.log('Você é Brasileiro!')
+}
+else {
+    console.log('Você é Estrangeiro!')
+}
