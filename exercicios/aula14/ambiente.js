6ca3783fc130e9cca7c04e69a579eb0a7327be74
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2023-12-07 20:43:55 -0300

    Criei o ex 14

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,8 @@
+console.log("Vai Começa...");
+for(var i = 1; i <= 4; i++) {
+   console.log(i);
+}
+ 
+console.log('Fim!');
+
+
---
+++ exercicios/aula14/node_163db3ecf56c8.tmp.js
@@ -0,0 +1,9 @@
+// for(var i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+var i = 1;
+
+while(i = 1){
+    console.log("Fudeo");
+}
