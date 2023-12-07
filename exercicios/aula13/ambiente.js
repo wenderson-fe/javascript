0e61b1be13318b91a4d3323a43ecfea6482946fa
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2023-12-07 20:25:37 -0300

    criei o ex 13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var i = 1;
+
+do{
+    console.log(`Passo ${i}`);
+    i++;
+} while(i <= 6)
+
+
+/*
+while(i <= 6) {
+    console.log(`Passo ${i}`);
+    i++;
+}
+*/
+
+
+/*
+console.log("Tudo Bem!");
+console.log("Tudo Bem!");
+console.log("Tudo Bem!");
+console.log("Tudo Bem!");
+console.log("Tudo Bem!");
+*/
+
