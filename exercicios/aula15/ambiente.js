f4993595caebe6ea4df052bbb6cb97b822de3c35
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2023-12-09 19:41:27 -0300

    Criei o exercício 15

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,19 @@
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var pos = num.indexOf(7);
+if (pos == -1){
+    console.log('O valor não foi encontrado');
+} else {
+    console.log(`O valor está na posição ${pos}`);
+}
+
+
+// for(var i in num){
+//     console.log(`A posição ${i} tem o valor ${num[i]}`);
+// }
+
+// for(var i = 0; i < num.length; i++){
+//     console.log(`A posição ${i} tem o valor ${num[i]}`);
+// }
+
+
+
